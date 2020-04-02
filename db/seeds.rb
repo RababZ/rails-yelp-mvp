bd2ba70470977adf75ba41e4d435ca5f70bc6769
Rabab Zaitouni <rabab.zaitouni@gmail.com>
2020-04-02 14:23:08 +0100

    updated ruby version

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Grand Bistrot',
+    address:      '40 Quai Henri 4, Paris 75004',
+    phone_number: '0785347203',
+    category:     'french'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '0984583484',
+    category:     'italian'
+  },
+  {
+    name:         'Crazy fries',
+    address:      '13 Rue Jacques Brel, Belgium 7JE',
+    phone_number: '0783746384',
+    category:     'belgian'
+  },
+  {
+    name:         'Shanghai',
+    address:      '2 Avenue Sully, Paris 75001',
+    phone_number: '07837463484',
+    category:     'chinese'
+  },
+  {
+    name:         'JapJap',
+    address:      '14 Avenue Zerektouni, Casablanca',
+    phone_number: '0528838482',
+    category:     'japanese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
